c874cacbcfcf569dc4c4e302923fb2f7a63e14d9
FarmingtonS9 <sframpton40@gmail.com>
2021-12-19 04:22:22 +1100

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    println!("Hello, world!");
+    collatz_sequence(500);
+}
+
+fn collatz_sequence (mut x: i32) -> i32 {
+    while x != 1{
+        if x == 1 {
+            println!("{}", x);
+        } else if x % 2 == 0 {
+            x = x / 2;
+            println!("{}", x);
+        } else {
+            x = (x * 3) + 1;
+            println!("{}", x);
+        }
+    }
+    return 0
 }
